1fa80563192a1db7afc2a78cd4d62dfbde466500
richsilv@yahoo.co.uk <richardlsilverton32@googlemail.com>
2015-02-24 11:37:25 +0000

    updated mini-landing-page

--- scripts/homepage-anim.js
+++ scripts/homepage-anim.js
@@ -1,4 +1,9 @@
 $(document).ready(function() {
+
+    var LOIS_BUTTON_SIZE = 6.7,
+        LOIS_BUTTON_BORDER = 0.3,
+        TOP_MENU_HEIGHT = 4.6;
+
     var controller = new ScrollMagic(),
         controller2 = new ScrollMagic(),
         // topSectionPin,
@@ -19,8 +24,7 @@ $(document).ready(function() {
                 // ScrollMagic will not calculate the topbar spacer correctly as it is a different height on
                 // initialisation from when we need it to be fixed and the rest of the page scroll.
                 // So we need to update the minimum height of the spacer based on the CSS:
-                // spacerHeight = topBarHeight + whiteSpaceHeight = 5.3vmax + (1.075 * 5.3vmax) = 2.075 * 5.3 * vmax
-                $('.topSectionSpacer').css('min-height', 2.075 * 5.3 * vmax);
+                $('.topSectionSpacer').css('min-height', TOP_MENU_HEIGHT * vmax);
             }
         });
 
@@ -32,20 +36,20 @@ $(document).ready(function() {
         .setTween(TweenLite.to('.lois-blurb', 1, {opacity: 0}) )
         .addTo(controller);
     new ScrollScene({offset: 200, duration: 800})
-        .setTween(TweenLite.to('.top-spacer', 2, {height: 5.6975 * vmax}) )
+        .setTween(TweenLite.to('.top-spacer', 2, {height: 0}) )
         .addTo(controller);
     new ScrollScene({offset: 200, duration: 800})
-        .setTween(TweenLite.to('.top-menu .centered-button', 2, {height: 10.5 * vmax, width: 10.5 * vmax}) )
+        .setTween(TweenLite.to('.top-menu .centered-button', 2, {height: LOIS_BUTTON_SIZE * vmax, width: LOIS_BUTTON_SIZE * vmax}) )
         .addTo(controller);
     new ScrollScene({offset: 200, duration: 800})
-        .setTween(TweenLite.to('.top-menu .centered-button .button-text', 2, {'font-size': 2.3 * vmax}) )
+        .setTween(TweenLite.to('.top-menu .centered-button .button-text', 2, {'font-size': LOIS_BUTTON_SIZE * 0.26 * vmax}) )
         .addTo(controller);
 
     new ScrollScene({offset: 200, duration: 800})
-        .setTween(TweenLite.to('.top-menu', 1, {height: 5.3 * vmax}))
+        .setTween(TweenLite.to('.top-menu', 1, {height: TOP_MENU_HEIGHT * vmax}))
         .addTo(controller);
     new ScrollScene({offset: 200, duration: 800})
-        .setTween(TweenLite.to('.top-menu .centered-button', 1, {top: 2.65 * vmax}))
+        .setTween(TweenLite.to('.top-menu .centered-button', 1, {top: ((LOIS_BUTTON_SIZE * 0.5) + LOIS_BUTTON_BORDER) * vmax}))
         .addTo(controller);
 
     new ScrollScene({offset: 1000, duration: 600})
