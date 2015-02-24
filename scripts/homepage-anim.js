179f19c472947f469dcd04d68c6e4037135be7cc
richsilv@yahoo.co.uk <richardlsilverton32@googlemail.com>
2015-02-24 15:18:02 +0000

    added responsive css styles

--- scripts/homepage-anim.js
+++ scripts/homepage-anim.js
@@ -10,7 +10,7 @@ $(document).ready(function() {
         vw = $(window).width() / 100,
         vh = $(window).height() / 100,
         vmax = Math.max(vw, vh),
-        topSectionFactor = (vh > vw && vw <= 3.2) ? 21.5 : 21.5,
+        topSectionFactor = (vh > vw && vw <= 3.2) ? 30.5 : 21.5,
         tl = new TimelineLite({
             paused: true,
             onComplete: function() {
