ca172df4744397e99763c1aa5619dd43f1649429
richsilv@yahoo.co.uk <richardlsilverton32@googlemail.com>
2015-03-17 19:58:08 +0000

    tweaked js for landing page

--- scripts/homepage-anim.js
+++ scripts/homepage-anim.js
@@ -8,7 +8,7 @@ $(document).ready(function() {
         // topSectionPin,
         vw = $(window).width() / 100,
         vh = $(window).height() / 100,
-        vmax = Math.max(vw, vh),
+        vmax = Math.max(vw, vh, 12.8),
         topSectionFactor = (vh > vw && vw <= 4) ? 30.5 : 21.5,
         tl = new TimelineLite({
             paused: true,
